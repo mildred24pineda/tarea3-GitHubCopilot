b2cbeb3793d30a231e4ebfd3afae311d70e88e5e
mildred24pineda <mpinedap3@miumg.edu.gt>
2025-02-07 17:32:54 -0600

    Add files via upload

---
+++ programa1.py
@@ -0,0 +1,11 @@
+# Solicitar al usuario que ingrese el primer número
+num1 = float(input("Ingrese el primer número: "))
+
+# Solicitar al usuario que ingrese el segundo número
+num2 = float(input("Ingrese el segundo número: "))
+
+# Sumar los dos números
+suma = num1 + num2
+
+# Mostrar el resultado de la suma
+print("La suma de los dos números es:", suma)
